ced7ae8ea83194fcd99e7950d2cf9085b99c0452
Adem ilter <ailter@protel.com.tr>
2016-04-15 09:14:34 +0300

    demo edit

--- examples/demo.js
+++ examples/demo.js
@@ -3,7 +3,7 @@ function newBox() {
       return i > 5 ? null : a[Math.floor(Math.random() * 16)]
     }).join('');
 
-  var heights = [30, 50, 80, 100, 130, 180];
+  var heights = [50, 90, 150, 190, 230];
   var randomHeight = heights[Math.floor(Math.random() * heights.length)];
 
   var $Box = $("<div class='box' />").css({
